ec2209eeb92599698052268a380f4221ff8d2c4a
OlexandrSolo <coldfellow20111@gmail.com>
2025-10-03 17:41:38 +0300

    start work with module2, finished task1

---
+++ js/module-2/task-1.js
@@ -0,0 +1,21 @@
+const makeTransaction = (quantity, priceDroid, customerCredits) => {
+
+    if (isNaN(quantity) || isNaN(priceDroid) || isNaN(customerCredits)) {
+        return console.log("Invalid date");
+    }
+    const totalPrice = quantity * priceDroid;
+
+    if (totalPrice > customerCredits) {
+        return console.log("Insufficient funds!");
+    }
+    return console.log(`Ypu ordered ${quantity} droids wroth ${totalPrice} credits`);
+}
+
+
+// TEST
+
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
