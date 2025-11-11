d0e496d16740602193fc65ab06bd52a50c94e44d
OlexandrSolo <coldfellow20111@gmail.com>
2025-11-11 10:07:42 +0200

    finished task-3

--- js/module-4/task-3.js
+++ js/module-4/task-3.js
@@ -2,16 +2,29 @@ const profile = {
     username: "Jacob",
     playTime: 300,
 
-    changeUserName(newName) {
+    changeUsername(newName) {
         if (typeof newName === "string" || newName.length >= 4) {
             this.username = newName;
         }
         return "This name is incorrect"
-    }
+    },
 
-    udatePlayTime(hours) {
+    updatePlayTime(hours) {
         if (typeof hours === "number" || hours >= 0) {
-            this.playTime = hours
+            this.playTime += hours
         }
+    },
+
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours!`
     }
 };
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
