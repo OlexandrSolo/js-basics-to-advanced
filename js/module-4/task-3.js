a75347d11164c02ef72fd7ef64faa22b47bb731e
OlexandrSolo <coldfellow20111@gmail.com>
2025-11-17 21:40:37 +0200

    fix bug

--- js/module-4/task-3.js
+++ js/module-4/task-3.js
@@ -28,3 +28,17 @@ console.log(profile.getInfo()); // "Marco has 300 active hours!"
 
 profile.updatePlayTime(20);
 console.log(profile.getInfo()); // "Marco has 320 active hours!"
+
+const str = "sdnfdfkjsdaqwdkdnvsd";
+
+function countChars(str) {
+    const array = str.split("");
+    const result = array.reduce((acc, item) => {
+        acc[item] = (acc[item] || 0) + 1;
+
+        return acc
+    }, {})
+    return result
+}
+
+console.log(sizeString(str));
