f3d64213fff9e3d6bfc1c37f945d533957c762b7
OlexandrSolo <coldfellow20111@gmail.com>
2025-12-12 08:48:10 +0200

    completed all tasks module 7

---
+++ js/module-7/task-5.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+
+button.addEventListener('click', () => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    colorSpan.textContent = document.body.style.backgroundColor;
+});
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+        .toString(16)
+        .padStart(6, 0)}`;
+}
